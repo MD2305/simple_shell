99754c11d4427c93c06db7fa704165af4933f586
MD2305 <drissmellouki96@gmail.com>
2023-12-11 06:01:23 -0800

    create header file main and include std

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
